1e6349d1ae6f9159a86e45cc2c433cc97e865ea9
hengyinayo <wuheng@yonsei.ac.kr>
2024-06-27 07:23:54 +0900

    feat: 레시피 검색

--- GustomateApp/recipe/CRUD.py
+++ GustomateApp/recipe/CRUD.py
@@ -6,3 +6,6 @@ def get_recipes(db: Session, skip: int = 0, limit: int = 100):
 
 def get_recipe_review(db: Session, recipe_id: int):
     return db.query(Recipe_Reviews).filter(Recipe_Reviews.recipe_id == recipe_id).all()
+
+def search_recipes(db: Session, query: str):
+    return db.query(Recipes).filter(Recipes.recipe_name.ilike(f"%{query}%")).all()
--- GustomateApp/recipe/router.py
+++ GustomateApp/recipe/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from .schema import Recipe, RecipeReviews
-from .CRUD import get_recipes, get_recipe_review
+from .CRUD import get_recipes, get_recipe_review, search_recipes
 from ..dependency.dependencies import get_db 
 from ..database import models
 
@@ -13,6 +13,16 @@ router = APIRouter()
 async def get_recipe_list(skip: int = 0, limit: int = 6, db: Session = Depends(get_db)):
     recipes = get_recipes(db, skip=skip, limit=limit)
     return recipes
+
+
+# 레시피 검색
+@router.get("/recipes/search", response_model=List[Recipe])
+async def search_recipe(query: str, db: Session = Depends(get_db)):
+    recipes = search_recipes(db, query)
+    if not recipes:
+        raise HTTPException(status_code=404, detail="No recipes found for the search query")
+    return recipes
+
 
 # 레시피 상세 정보 및 리뷰
 @router.get("/recipes/{recipe_id}", response_model=List[RecipeReviews])
