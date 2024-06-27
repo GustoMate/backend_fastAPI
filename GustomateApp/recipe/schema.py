19d6f54281ff00f2c43f2283c1dec82244d13c7e
hengyinayo <wuheng@yonsei.ac.kr>
2024-06-28 03:45:02 +0900

    feat: 데이터베이스 업데이트

--- GustomateApp/recipe/schema.py
+++ GustomateApp/recipe/schema.py
@@ -6,13 +6,22 @@ class Recipe(BaseModel):
     recipe_id: int
     recipe_name: str
     image: str
-    difficulty: int
-    steps: str
-    cuisine_id: int
+    method_classification: str
+    country_classification: str
+    theme_classification: str
+    difficulty_classification: str
+    calorie: int
+    view: int
+    quantity: int
+    main_ingredients: str
+    sub_ingredients: str
+    seasonings: str
+    recipe: str
+    cooking_time: int
     spiciness: int
     created_at: datetime
     updated_at: datetime
-    
+
 
 class RecipeReviews(BaseModel):
     review_id: int
@@ -23,3 +32,5 @@ class RecipeReviews(BaseModel):
     rating: int
     created_at: datetime
     updated_at: datetime
+
+
